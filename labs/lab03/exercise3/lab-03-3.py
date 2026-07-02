80743a2fbb3c75ada18fad7843adaf4fa4457350
syazwana2122 <azwnna@gmail.com>
2026-07-02 15:15:03 +0800

    changes in lab 03 exercise 03

---
+++ labs/lab03/exercise3/lab-03-3.py
@@ -0,0 +1,9 @@
+kilobyte = float(input())
+megabyte = kilobyte / 1000
+print(megabyte)
+gigabyte = kilobyte / 1000 * 1000
+print(gigabyte)
+terabyte = kilobyte / 1000 * 1000 * 1000
+print(terabyte)
+petabyte = kilobyte / 1000 * 1000 * 1000 * 1000
+print(petabyte)
