21c95fcd0373c09be26c0a5d092e0be038ab1d6d
syazwana2122 <azwnna@gmail.com>
2026-07-14 11:30:26 +0800

    test

--- labs/lab03/exercise3/lab-03-3.py
+++ labs/lab03/exercise3/lab-03-3.py
@@ -1,10 +1,13 @@
-kilobyte = float(input())
-megabyte = kilobyte / 1024
-print(megabyte)
-gigabyte = kilobyte / 1024 * 1024
-print(gigabyte)
-terabyte = kilobyte / 1024 * 1024 * 1024
-print(terabyte)
-petabyte = kilobyte / 1024 * 1024 * 1024 * 1024
-print(petabyte)
+# lab-03-3.py
 
+kilobyte = float(input())
+
+megabyte = kilobyte / 1024
+gigabyte = megabyte / 1024
+terabyte = gigabyte / 1024
+petabyte = terabyte / 1024
+
+print(megabyte)
+print(gigabyte)
+print(terabyte)
+print(petabyte)
--- labs/lab03/exercise7/lab-03-7.py
+++ labs/lab03/exercise7/lab-03-7.py
@@ -1,5 +1,6 @@
 celsius = float(input())
 fahrenheit = celsius * float(9) / 5 + 32
 print(fahrenheit)
-kelvin = celsius + 273
+kelvin = celsius + 273.15
 print(kelvin)
+
