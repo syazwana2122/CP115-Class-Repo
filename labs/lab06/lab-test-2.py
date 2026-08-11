5f0a8af50703ae2da6e1eb2573525502c858091e
syazwana2122 <azwnna@gmail.com>
2026-08-11 11:18:02 +0800

    Lab Test 2

---
+++ labs/lab06/lab-test-2.py
@@ -0,0 +1,16 @@
+Name = "Ahmad Bin Abu"          # Programmer's Name
+Matric_Number = "MS2025123499"  # Programmer's Matric Number
+Marks = 2 * 10                  # Assignment Full Marks
+
+print (f"Name: {Name}\t\t\tMatric. No: {Matric_Number}\n\n" # Display Programmer's Name and Matric Number
+       f"*\t\t\t*\n"                                         
+       f"**\t\t**\n"
+       f"***\t***\n"
+       f"********\n"
+       f"***\t***\n"
+       f"**\t\t**\n"
+       f"*\t\t\t*\n\n"
+       f"This is my\n"  
+       f"\tsecond\n"
+       f"\t\tassignment\n"
+       f"I want 2x10 marks, which is {Marks} full marks\n")  # Display Assignment Full Marks Using An Arithmetic Expression
