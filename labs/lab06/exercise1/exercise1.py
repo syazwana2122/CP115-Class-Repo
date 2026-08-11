d8409383963e42d8d9f7270d9890018eef83bc81
syazwana2122 <azwnna@gmail.com>
2026-08-11 10:28:30 +0800

    Complete Lab 6: Escape Characters and Comments

--- labs/lab06/exercise1/exercise1.py
+++ labs/lab06/exercise1/exercise1.py
@@ -2,3 +2,21 @@
 # Print the receipt shown in the lab, using \n for new lines and \t for columns.
 # Calculate every total, subtotal, and tax in your code. Do not type the money
 # amounts in directly. Show every amount with exactly two decimal places.
+# Without \n - everything prints on one line
+Coffee_Total = 3.50 * 2
+Muffin_Total = 2.10 * 3
+Water_Total = 1.05 * 4
+Sub_Total = Coffee_Total + Muffin_Total + Water_Total
+Tax = Sub_Total * 0.06
+Total = Tax + Sub_Total
+print(
+    f"========== RECEIPT ==========\n",
+    f"Item\tPrice\tQty\tTotal\n",
+    f"Coffee\t$3.50\t2\t${Coffee_Total}\n",
+    f"Muffin\t$2.10\t3\t${Muffin_Total}\n",
+    f"Water\t$1.05\t4\t${Water_Total}\n",
+    f"------------------------------\n",
+    f"Subtotal\t\t${Sub_Total}\n",
+    f"Tax(6%)\t\t${Tax}\n",
+    f"Total\t\t${Total}",
+)
