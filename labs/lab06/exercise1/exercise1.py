343bbca10e56a3f0e6f743d1f901a11726e9be11
syazwana2122 <azwnna@gmail.com>
2026-08-11 10:31:44 +0800

    Complete Lab 6: Escape Characters and Comments

--- labs/lab06/exercise1/exercise1.py
+++ labs/lab06/exercise1/exercise1.py
@@ -3,6 +3,9 @@
 # Calculate every total, subtotal, and tax in your code. Do not type the money
 # amounts in directly. Show every amount with exactly two decimal places.
 # Without \n - everything prints on one line
+# Print the receipt shown in the lab, using \n for new lines and \t for columns.
+# Calculate every total, subtotal, and tax in your code. Do not type the money
+# amounts in directly. Show every amount with exactly two decimal places.
 Coffee_Total = 3.50 * 2
 Muffin_Total = 2.10 * 3
 Water_Total = 1.05 * 4
